ab174438dce7b99606a008c8334a74fc275ccf39
lashu <lashurov44@gmail.com>
2026-01-12 19:45:08 +0200

    Optimize `findPublishersByAuthor` with Sequelize aggregation and distinct query

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -6,17 +6,17 @@ export const findPublishersByAuthor = async (req, res) => {
     if (!author) {
         return res.status(404).send({error: `Author with name ${req.params.name} not found`});
     }
-    const book = await Book.findAll({
-        include:{
-            model: Author, as: 'authors',
-            where: {name: req.params.name}
-            ,through: {attributes: []}
-        },
-        attributes: ['publisher'],
-        raw: true,
-        group: ['publisher']
-    });
-    const publishers = book.map(b => b.publisher);
-    return res.json(publishers)
+
+    const publishers = await Book.aggregate('publisher', 'DISTINCT',
+        {
+            plain:false,
+            include:{
+                model:author,
+                as:'authors',
+                where:{name: author.name},
+                through:{attributes: []}
+            }
+        });
+    return res.json(publishers.map(p => p.DISTINCT));
 
 }
